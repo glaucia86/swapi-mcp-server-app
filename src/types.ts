67e8730980438edc72cc3e033b1a9492f7f8b711
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2025-10-04 18:00:08 -0300

    feat: add TypeScript interfaces for People, Planets, Films, and SearchResponse

---
+++ src/types.ts
@@ -0,0 +1,59 @@
+export interface People {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  homeworld: string;
+  films: string[];
+  species: string[];
+  vehicles: string[];
+  starships: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
+
+export interface Planets {
+  name: string;
+  diameter: string;
+  rotation_period: string;
+  orbital_period: string;
+  gravity: string;
+  population: string;
+  climate: string;
+  terrain: string;
+  surface_water: string;
+  residents: string[];
+  films: string[];
+  url: string;
+  created: string;
+  edited: string;
+}
+
+export interface Films {
+  title: string;
+  episode_id: number;
+  opening_crawl: string;
+  director: string;
+  producer: string;
+  release_date: string;
+  species: string[];
+  starships: string[];
+  vehicles: string[];
+  characters: string[];
+  planets: string[];
+  url: string;
+  created: string;
+  edited: string;
+}
+
+export interface SearchResponse<T> {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: T[];
+}
